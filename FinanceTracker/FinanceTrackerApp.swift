653da62b4408ef6aac77a85273486ad897356c64
Hediye Sert <hediye-sert@hotmail.com>
2026-05-14 13:07:10 +0300

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by hediye sert on 14.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
